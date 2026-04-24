fce164a0e7d2f60b67b9221b5c4ca4b859c64e81
estudioescritaplanejada-creator <estudioescritaplanejada@gmail.com>
2026-04-24 12:11:13 -0300

    Site 100% organizado na raiz

--- build-sitemap.sh
+++ build-sitemap.sh
@@ -4,16 +4,12 @@
 URL_BASE="https://estudioescritaplanejada-creator.github.io/site"
 ARQUIVO_SITEMAP="sitemap.xml"
 
-echo "Gerando sitemap..."
+echo "Gerando sitemap na raiz..."
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" > $ARQUIVO_SITEMAP
 echo "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">" >> $ARQUIVO_SITEMAP
 
-# Adiciona o index da raiz
-echo "  <url><loc>$URL_BASE/index.html</loc></url>" >> $ARQUIVO_SITEMAP
-
-# Procura todos os .html dentro da pasta 'paginas' e subpastas
-# Usei 'paginas' pois vi que é o nome da sua pasta no ls -la
-find paginas -name "*.html" | while read -r linha; do
+# Procura todos os .html na PASTA ATUAL (raiz), ignorando subpastas
+find . -maxdepth 1 -name "*.html" | sed 's|^\./||' | while read -r linha; do
     echo "  <url><loc>$URL_BASE/$linha</loc></url>" >> $ARQUIVO_SITEMAP
 done
 
